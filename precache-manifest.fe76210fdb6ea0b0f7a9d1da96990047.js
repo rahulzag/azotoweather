60556ed584ed47e334ed3cef8725eef80d9d8510
rahulzag <rahulzag23@gmail.com>
2020-09-28 16:11:57 +0530

    Updates

---
+++ precache-manifest.fe76210fdb6ea0b0f7a9d1da96990047.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4f083d44b05503f87a86d99bfd70ca0",
+    "url": "/azotoweather/index.html"
+  },
+  {
+    "revision": "1c516ca923f4ef1b4b33",
+    "url": "/azotoweather/static/css/main.e4364eac.chunk.css"
+  },
+  {
+    "revision": "bfcc1e32cb8c7d9af143",
+    "url": "/azotoweather/static/js/2.10b0ca7b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/azotoweather/static/js/2.10b0ca7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c516ca923f4ef1b4b33",
+    "url": "/azotoweather/static/js/main.29833501.chunk.js"
+  },
+  {
+    "revision": "372ad7a7289a0afee98d",
+    "url": "/azotoweather/static/js/runtime-main.5be44da7.js"
+  },
+  {
+    "revision": "c97cb84b74a52edcb496295687e87822",
+    "url": "/azotoweather/static/media/cold-bg.c97cb84b.jpg"
+  },
+  {
+    "revision": "635ed87d2cc826fb213057ac40dfaff1",
+    "url": "/azotoweather/static/media/warm-bg.635ed87d.jpg"
+  }
+]);
